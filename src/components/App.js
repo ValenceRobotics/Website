a94e18efac4604acfcbffa8c5baa4fa69a002883
Jacob <jacobvm04@gmail.com>
2021-12-12 18:22:44 -0500

    Navbar setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
 import Home from "./Home";
 import About from "./About";
 
 function App() {
   return (
     <BrowserRouter>
+        <Navbar/>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
