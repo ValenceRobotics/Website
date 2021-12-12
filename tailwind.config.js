d4c35f34c8370c9c3728d5bd70bd1066d21ffc8a
Jacob <jacobvm04@gmail.com>
2021-12-12 18:33:54 -0500

    Added font and color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-orange': '#F79B4C',
+        'dark-orange': '#F47318',
+        'footer': '#FFDA7B',
+      },
+      fontFamily: {
+        sans: ['Roboto'],
+      }
+    },
   },
   plugins: [],
 }
