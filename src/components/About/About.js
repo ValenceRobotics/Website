39666007060b783ba014431cf3e0100ee77398a6
Jacob <jacobvm04@gmail.com>
2021-12-13 15:45:58 -0500

    Added interested in joining

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,3 +1,4 @@
+import InterestedInJoining from "./InterestedInJoining";
 import Jumbotron from "./Jumbotron";
 import OurTeam from "./OurTeam";
 import WhatIsFirst from "./WhatIsFirst";
@@ -11,6 +12,8 @@ function About() {
         <OurTeam />
         <div className="mt-20" />
         <WhatIsFirst />
+        <div className="mt-20" />
+        <InterestedInJoining />
       </div>
     </div>
   );
---
+++ src/components/About/InterestedInJoining.js
@@ -0,0 +1,17 @@
+function InterestedInJoining() {
+    return (
+        <div>
+            <div className="flex justify-evenly">
+                <h1 className="text-4xl font-bold text-center text-light-orange">Interested <span className="text-black">in</span> Joining?</h1>
+            </div> 
+            <div className="flex justify-center mt-8 items-center gap-x-4 gap-y-4 flex-wrap">
+                <div className="bg-light-orange rounded-full w-14 h-14 flex justify-center items-center">
+                    <h1 className="text-4xl md:text-5xl font-bold text-center text-white ">1</h1>
+                </div>
+                <h2 className="text-3xl md:text-4xl font-bold text-center text-black align-middle">Fill out the <a href="" className="text-light-orange underline hover:text-dark-orange">member information form</a></h2>
+            </div> 
+        </div>
+    );
+}
+
+export default InterestedInJoining;
