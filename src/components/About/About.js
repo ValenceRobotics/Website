9bc735e9a6b248f34a0cba07df248abe1aa36906
Ganning Xu <ganningxu@gmail.com>
2022-11-22 11:06:58 -0500

    update contact form link

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ import OurTeam from "./OurTeam";
 import WhatIsFirst from "./WhatIsFirst";
 import { Navbar } from "../NavBar";
 import Banner from "../Banner";
-import { SimpleGrid } from '@chakra-ui/react'
+import { SimpleGrid } from "@chakra-ui/react";
 import { GoMarkGithub } from "react-icons/go";
 import { AiOutlineInstagram } from "react-icons/ai";
 import { BsPaypal } from "react-icons/bs";
@@ -24,22 +24,57 @@ function About() {
         <div className="container mx-auto">
           <Jumbotron />
           <div className="mt-20" />
-          <WhatIsFirst heading="What is FIRST?">FIRST® (For Inspiration and Recognition of Science and Technology) is a non-profit organization dedicated to advancing STEM education in the next generation of students. FIRST® Robotics Competition is one of the programs– alongside FIRST® LEGO® League, FIRST® Tech Challenge, and others– aimed specifically at high school students.
-            Find out more <a href="https://www.firstinspires.org/" target="_blank" rel="noreferrer" className="text-blue-500 hover:text-blue-900">here</a></WhatIsFirst>
+          <WhatIsFirst heading="What is FIRST?">
+            FIRST® (For Inspiration and Recognition of Science and Technology)
+            is a non-profit organization dedicated to advancing STEM education
+            in the next generation of students. FIRST® Robotics Competition is
+            one of the programs– alongside FIRST® LEGO® League, FIRST® Tech
+            Challenge, and others– aimed specifically at high school students.
+            Find out more{" "}
+            <a
+              href="https://www.firstinspires.org/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-blue-500 hover:text-blue-900"
+            >
+              here
+            </a>
+          </WhatIsFirst>
           <div className="mt-20" />
           <OurTeam />
           <div className="mt-20" />
           <InterestedInJoining />
           {/* <MeetTheTeam /> */}
 
-          <SimpleGrid columns={{ base: 1, md: 2 }} mt="20" mx="auto" spacing="10">
+          <SimpleGrid
+            columns={{ base: 1, md: 2 }}
+            mt="20"
+            mx="auto"
+            spacing="10"
+          >
             <div className="mx-auto" alignSelf="center">
-              <h1 className="text-4xl font-bold text-light-orange font-heading">Contact <span className="text-black dark:text-slate-200">Us</span></h1>
-              <p className="italic mt-1">Feel free to reach out!</p>
+              <h1 className="text-4xl font-bold text-light-orange font-heading">
+                Contact{" "}
+                <span className="text-black dark:text-slate-200">Us</span>
+              </h1>
+              <p className="italic mt-1">
+                <a
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSeLQs0lQfWemMqAMV-yV4p-2MpGGPK2U-7s2skTfnoMSWUepQ/viewform"
+                  className="text-dark-orange underline hover:text-dark-orange"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Feel free to reach out!
+                </a>
+              </p>
 
-              <p className=" mt-3 text-xl font-para"><span className="font-bold">Phone: </span>(919) 886-4388</p>
-              <p className="  text-xl font-para"><span className="font-bold">Email: </span>leadership@valencerobotics.org</p>
-              <p className="  text-xl font-para"><span className="font-bold">Location: </span>Durham, NC</p>
+              <p className=" mt-3 text-xl font-para">
+                <span className="font-bold">Phone: </span>(919) 886-4388
+              </p>
+              {/* <p className="  text-xl font-para"><span className="font-bold">Email: </span>leadership@valencerobotics.org</p> */}
+              <p className="  text-xl font-para">
+                <span className="font-bold">Location: </span>Durham, NC
+              </p>
 
               <div className="mt-4">
                 {/* <IconContext.Provider value={{ className: "text-3xl text-dark-orange" }}>
@@ -54,34 +89,66 @@ function About() {
                   </a>
                 </IconContext.Provider> */}
 
-                <IconContext.Provider value={{ className: "text-3xl text-light-orange" }}>
-                  <a href="https://github.com/ValenceRobotics" target="_blank" rel="noopener noreferrer">
+                <IconContext.Provider
+                  value={{ className: "text-3xl text-light-orange" }}
+                >
+                  <a
+                    href="https://github.com/ValenceRobotics"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <div className="mt-3">
                       <GoMarkGithub className="inline" />
-                      <p className="inline text-lg font-semibold"> ValenceRobotics</p>
+                      <p className="inline text-lg font-semibold">
+                        {" "}
+                        ValenceRobotics
+                      </p>
                     </div>
                   </a>
                   <div />
-                  <a href="https://www.instagram.com/valence_robotics/" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://www.instagram.com/valence_robotics/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <div className="mt-3">
                       <AiOutlineInstagram className="inline " />
-                      <p className="inline text-lg font-semibold"> @valence_robotics</p>
+                      <p className="inline text-lg font-semibold">
+                        {" "}
+                        @valence_robotics
+                      </p>
                     </div>
                   </a>
                   <div />
-                  <a href="https://www.paypal.com/paypalme/valencerobotics" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://www.paypal.com/paypalme/valencerobotics"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <div className="mt-3">
                       <BsPaypal className="inline" />
-                      <p className="inline text-lg font-semibold"> @valencerobotics</p>
+                      <p className="inline text-lg font-semibold">
+                        {" "}
+                        @valencerobotics
+                      </p>
                     </div>
                   </a>
                 </IconContext.Provider>
               </div>
             </div>
 
-            <iframe title="asdf" width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Durham,%20NC%2027705+(Your%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/sport-gps/">bike gps</a></iframe>
-
-
+            <iframe
+              title="asdf"
+              width="100%"
+              height="400"
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+              src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Durham,%20NC%2027705+(Your%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+            >
+              <a href="https://www.gps.ie/sport-gps/">bike gps</a>
+            </iframe>
           </SimpleGrid>
         </div>
       </div>
