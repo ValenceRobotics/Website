c1a419e33bffdd64f05e4519cb015e11d2cf2948
Ayush <61765149+citricacid2@users.noreply.github.com>
2023-04-24 18:40:49 -0400

    j

--- src/components/About/InterestedInJoining.js
+++ src/components/About/InterestedInJoining.js
@@ -6,7 +6,7 @@ function InterestedInJoining() {
       <SimpleGrid columns={{ base: 1, md: 1 }} mt="20" mx="auto" spacing="10">
         <h1 className="text-4xl font-bold text-black font-heading mx-auto dark:text-slate-200">
           <span className="text-dark-orange">Interested</span> in{" "}
-          <span className="text-dark-orange">Joining</span>? Fill out the{" "}
+          <span className="text-dark-orange">joining</span>? Fill out the{" "}
           <a
             href="https://forms.gle/XDHLhtjhZRDmcG6q8"
             className="text-dark-orange underline hover:text-dark-orange"
